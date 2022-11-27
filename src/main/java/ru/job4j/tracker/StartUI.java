9ce2c4f192bd66e5d61feb3a0f1cd490b7c2d7a5
IrinZay <i_m_k@vk.ru>
2022-11-27 11:14:04 +0300

    8. Date. Отображение даты.

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        System.out.println(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss").format(item.getCreated()));
+    }
+}
