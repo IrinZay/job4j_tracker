a7f8d2deeed9d1e0d0fba86739e6711ce2df5a8d
IrinZay <i_m_k@vk.ru>
2022-11-30 12:27:14 +0300

    9.2. Тесты вывода на консоль

--- src/main/java/ru/job4j/tracker/ShowAllItemsAction.java
+++ src/main/java/ru/job4j/tracker/ShowAllItemsAction.java
@@ -26,5 +26,4 @@ public class ShowAllItemsAction implements UserAction {
         }
         return true;
     }
-
 }
--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -149,4 +149,5 @@ public class StartUITest {
                         + "Exit Program" + ln
         );
     }
+
 }
