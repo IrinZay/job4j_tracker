4f87340a1dc345f60dbd9f19d3422fb81cd881f5
IrinZay <i_m_k@vk.ru>
2022-12-01 15:51:01 +0300

    1. Singleton

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker;
+
+public final class SingleTracker {
+    private static Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+
+    public static Item add(Item item){
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return null;
+    }
+}
